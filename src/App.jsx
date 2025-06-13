4617a47a92cafd0141a3d713fd2b330e75950ed6
Alan Alves <alan.oliveira@teledatabrasil.com.br>
2025-06-13 13:39:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectSiteBar from "./components/ProjectSideBar";
+
+function App() {
+  return (
+    <main className="h-screen my-8 ">
+      <ProjectSiteBar />
+    </main>
+  );
+}
+
+export default App;
