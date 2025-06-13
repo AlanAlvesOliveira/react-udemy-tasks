79b69ca1bd02e7f3dc1f9fe9568595260e59160c
Alan Alves <alan.oliveira@teledatabrasil.com.br>
2025-06-13 14:30:53 -0300

    new components, Button, Input, newProject, NoProjectSelected, ProjectSideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSiteBar from "./components/ProjectSideBar";
 
 function App() {
+  const [projectsState, setProjecrsTate] = useState({
+    selectedProjetctId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProjectI() {
+    setProjecrsTate(prevState => {
+      return {
+        ...prevState,
+        selectedProjetctId: null,
+      }
+    });
+  }
+  let content;
+  if (projectsState.selectedProjetctId === null) {
+    content = (<NewProject />)
+  } else {
+    content = (<NoProjectSelected onStartAddProject={handleStartAddProjectI} />
+    )
+  }
+
   return (
-    <main className="h-screen my-8 ">
-      <ProjectSiteBar />
+    <main className="h-screen my-8 flex gap-8 ">
+      <ProjectSiteBar onStartAddProject={handleStartAddProjectI} />
+      {content}
     </main>
   );
 }
